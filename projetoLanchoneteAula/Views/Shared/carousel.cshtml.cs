97b66954759025bc05b0bb78700449a4a50ae6dd
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2023-05-10 16:19:19 -0300

    Subindo Projeto Lanchonete

---
+++ projetoLanchoneteAula/Views/Shared/carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projetoLanchoneteAula.Views.Shared
+{
+    public class carrosselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
